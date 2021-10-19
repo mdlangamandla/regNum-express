490b3437dee4b1855621628e0e7503db6f6079d8
mdlangamandla <mdlangamandla048@gmail.com>
2021-10-19 12:30:51 +0200

    fixed tests

--- test/reg.test.js
+++ test/reg.test.js
@@ -19,9 +19,9 @@ beforeEach(async ()=> {
 
 });
 
-describe('Registration number exercise', async () => {
+describe('Registration number tests', async () => {
 
-    it("It should be able to cahnge lowercase to uppercase when registration is been entered as 'ca 123 456'", async () => {
+    it("It should be able to cahnge/convert lowercase to uppercase when registration is been entered as 'ca 123 456'", async () => {
         beforeEach(async ()=> {
             // clean the tables before each test run
             await registration.resetDB();
@@ -31,7 +31,7 @@ describe('Registration number exercise', async () => {
 
         assert.deepEqual([{ "reg_num": 'CA 123 456' }], await registration.getRegList())
     })
-    it("It should be able to change lowercase to uppercase when registration is been entered as 'cA 246 891'", async () => {
+    it("It should be able to change/convert lowercase to uppercase when registration is been entered as 'cA 246 891'", async () => {
         beforeEach(async ()=> {
             // clean the tables before each test run
             await registration.resetDB();
@@ -51,15 +51,15 @@ describe('Registration number exercise', async () => {
 
         assert.deepEqual([{ "reg_num": 'CY 878 555' }], await registration.getRegList())
     })
-    it("It should be able to convert small letter to capital letter when registration is been entered as 'Cl 878 555'", async () => {
+    it("It should be able to change/convert lowercase to uppercase when the registration is entered as 'Cf 878 555'", async () => {
         beforeEach(async ()=> {
             // clean the tables before each test run
             await registration.resetDB();
 
         });
-        await registration.setReg({ registration: "Cl 878 555" })
+        await registration.setReg({ registration: "Cf 878 555" })
 
-        assert.deepEqual([{ "reg_num": 'CL 878 555' }], await registration.getRegList())
+        assert.deepEqual([{ "reg_num": 'CF 878 555' }], await registration.getRegList())
     })
     describe('Validating by regex', () => {
         it('Should return error message if the registration number is entered is wrong.', async () => {
@@ -122,8 +122,23 @@ describe('Registration number exercise', async () => {
             assert.deepEqual([{ "reg_num": 'CA 123 456' }, { "reg_num": 'CY 123 456' }], await registration.getRegList())
         })
     })
+
+
+    describe('Test filter function', ()=>{
+        it('It should return id = 1 if Cape Town registration number is inserted', async ()=>{
+            assert.deepEqual(1, await registration.getID("CA 123-456"))
+        })
+        it('It should return id = 2 if Bellville registration number is inserted', async ()=>{
+            assert.deepEqual(2, await registration.getID("CY 123-456"))
+        })
+        it('It should return id = 3 if Kuilsriver registration number is inserted', async ()=>{
+            assert.deepEqual(3, await registration.getID("CF 123-456"))
+        })             
+    })
+
+
     describe("Filter by town", async () => {
-            it('It should only display Cape Town registration if selected town is Cape Town.', async () => {
+            it('It should only display Cape Town registration if a selected town is Cape Town.', async () => {
             beforeEach(async ()=> {
                 // clean the tables before each test run
                 await registration.resetDB();
@@ -140,7 +155,7 @@ describe('Registration number exercise', async () => {
             assert.deepEqual([{ "reg_num": 'CA 878 555' }, { "reg_num": 'CA 878 565' }], await registration.forTown({ townName: 'CA' }))
         })
 
-        it('It should only display Bellville registration if selected town is Bellville.', async () => {
+        it('It should only display Bellville registration if a selected town is Bellville.', async () => {
             beforeEach(async ()=> {
                 // clean the tables before each test run
                 await registration.resetDB();
@@ -154,9 +169,9 @@ describe('Registration number exercise', async () => {
             await registration.setReg({ registration: "ca 878 555" })
             await registration.setReg({ registration: "cf 878 555" })
 
-            assert.deepEqual([{ "reg_num": 'CY 878 555'},{"reg_num": 'CY 878 565' }], await registration.forTown({ townName: 'CL' }))
+            assert.deepEqual([{ "reg_num": 'CY 878 555'},{"reg_num": 'CY 878 565' }], await registration.forTown({ townName: 'CY' }))
         })
-        it('It should display Kuilsriver registration if selected town is Kuilsriver.', async () => {
+        it('It should display Kuilsriver registration if a selected town is Kuilsriver.', async () => {
             beforeEach(async ()=> {
                 // clean the tables before each test run
                 await registration.resetDB();
@@ -172,17 +187,5 @@ describe('Registration number exercise', async () => {
 
             assert.deepEqual([{ "reg_num": 'CF 878 555' }], await registration.forTown({ townName: 'CF' }))
         })
-    })
-
-    describe('Test filter function', ()=>{
-        it('It should return id = 1 if Kuilsriver registration number is inserted', async ()=>{
-            assert.deepEqual(1, await registration.getID("CF 123-456"))
-        })
-        it('It should return id = 2 if Bellville registration number is inserted', async ()=>{
-            assert.deepEqual(2, await registration.getID("CY 123-456"))
-        })
-        it('It should return id = 3 if Cape Town registration number is inserted', async ()=>{
-            assert.deepEqual(3, await registration.getID("CA 123-456"))
-        })             
     })
 })
